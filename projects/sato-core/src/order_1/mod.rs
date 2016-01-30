05346e3b073ce1fab0361461a8ac025dda8259a1
Aster <aster@vers.site>
2016-01-30 21:33:44 +0000

    🔧 Block erroneous formulas

--- projects/sato-core/src/order_1/mod.rs
+++ projects/sato-core/src/order_1/mod.rs
@@ -2,8 +2,6 @@ use core::fmt::{Display, Formatter};
 use dashu::{base::SquareRoot, float::DBig, integer::IBig};
 use std::ops::{Add, Div, Mul};
 
-const YOTTA: i64 = 10939058860032000;
-
 /// Ramanujan Level1 Series
 #[derive(Copy, Clone, Debug)]
 pub struct RamanujanL1 {
@@ -18,34 +16,34 @@ impl Display for RamanujanL1 {
     fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
         write!(
             f,
-            "({}Sqrt[{}]/{})/({}-Sum[Product[(72j^3-108j^2+46j-5)/({}j^3),{{j,1,k}}]/({}k+{}),{{k,1,Infinity}}])",
-            self.a, self.b, self.c, self.d, YOTTA, self.e, self.d,
+            "({}Sqrt[{}])/({}-Sum[Product[(72j^3-108j^2+46j-5)/({}j^3),{{j,1,k}}]/({}k+{}),{{k,1,Infinity}}])",
+            self.a, self.b, self.d, self.c, self.e, self.d,
         )
     }
 }
 
 impl RamanujanL1 {
-    /// let `j = (1 + sqrt(-7)) / 2, tau = ?, a = ?, b = ?`
-    pub const J7: Self = Self { a: 5, b: 15, c: 1, d: 8, e: 63 };
-    /// let `j = (1 + sqrt(-7)) / 2, tau = ?, a = ?, b = ?`
-    pub const J11: Self = Self { a: 32, b: 2, c: 1, d: 15, e: 154 };
+    /// let `j = (1 + sqrt(-7)) / 2`
+    pub const J7: Self = unimplemented!(); //{5 I Sqrt[15], 1125/8, -8, -63}
     /// let `j = (1 + sqrt(-11)) / 2`
-    pub const J19: Self = Self { a: 96, b: 6, c: 1, d: 75, e: 1026 };
+    pub const J11: Self = unimplemented!(); // 32 I Sqrt[2], 4096/3, -15, -154
     /// let `j = (1 + sqrt(-19)) / 2`
-    pub const J27: Self = Self { a: 160, b: 30, c: 1, d: 279, e: 4554 };
+    pub const J19: Self = Self { a: 32, b: 6, c: 36864, d: 25, e: 342 };
+    /// let `j = (1 + sqrt(-27)) / 2`
+    pub const J27: Self = unimplemented!(); // 160Sqrt[30]/9, -512000, -31, -506
     /// let `j = (1 + sqrt(-43)) / 2`
-    pub const J43: Self = Self { a: 640, b: 15, c: 1, d: 789, e: 16254 };
+    pub const J43: Self = unimplemented!(); // 640Sqrt[15]/3, -36864000, -263, -5418
     /// let `j = (1 + sqrt(-67)) / 2`
-    pub const J67: Self = Self { a: 1760, b: 330, c: 1, d: 10177, e: 261702 };
+    pub const J67: Self = Self { a: 1760, b: 330, c: 6133248000, d: 10177, e: 261702 };
     /// let `j = (1 + sqrt(-163)) / 2`
-    pub const J163: Self = Self { a: 426880, b: 10005, c: 1, d: 13591409, e: 545140134 };
+    pub const J163: Self = Self { a: 426880, b: 10005, c: 10939058860032000, d: 13591409, e: 545140134 };
 }
 
 impl RamanujanL1 {
     fn binary_split(&self, j: i64, i: i64) -> (IBig, IBig, IBig) {
         if i == j + 1 {
             let pab = IBig::from(-(6 * j - 5) * (2 * j - 1) * (6 * j - 1));
-            let qab = IBig::from(j).pow(3).mul(YOTTA);
+            let qab = IBig::from(j).pow(3).mul(self.c);
             let rab = IBig::from(self.e * j + self.d).mul(&pab);
             (pab, qab, rab)
         }
--- projects/sato-core/src/order_1/readme.md
+++ projects/sato-core/src/order_1/readme.md
@@ -1,27 +1,133 @@
-考虑公式 
+考虑如下**一级拉马努金级数(Level-1 Ramanujan Series)**:
 
 $$
-\pi =\sum_{k=0}^{\infty }\left[{\frac {1}{16^{k}}}\left({\frac {4}{8k+1}}-{\frac {2}{8k+4}}-{\frac {1}{8k+5}}-{\frac {1}{8k+6}}\right)\right]
+\frac{1}{\pi }=\sum_{k=0}^∞(-1)^{k+1}\binom{2 k}{k} \binom{3 k}{k} \binom{6 k}{3 k}c^{-k-\frac{1}{2}} (a k+b)
 $$
 
-两边乘以 $16^d$ 并取小数, 则:
+其中 a, b, c 是三个未知整数, j 函数定义如下, 也是个整数
 
-$$
-\{16^d\pi\} = \left\{\sum _{k=0}^{\infty }\left(
-{\frac {16^{d-k} 4}{8k+1}}-
-{\frac {16^{d-k} 2}{8k+4}}-
-{\frac {16^{d-k}}{8k+5}}-
-{\frac {16^{d-k}}{8k+6}}
-\right)\right\}
-$$
-
-
-举个例子, $d = 4$ 时有:
 
 $$
 \begin{aligned}
-\pi &≈ \texttt{3.243f6a8885a368d31\ldots} \\
-16^4 \pi &≈ \texttt{3243f.6a8885a368d31\ldots} \\
-\left\{16^4 \pi\right\} &≈  \phantom{0000\,}\texttt{0.6a8885a368d31\ldots}
+c &= j\left(\frac{1+\sqrt{-τ}}{2}\right)\\
+j(τ)&=\left({\frac {E_{4}(\tau )}{η^{8}(τ)}}\right)^{3}={\frac {1}{q}}+744+196884q+21493760q^{2}+\cdots 
 \end{aligned}
 $$
+
+选取如下参数
+
+$$
+\left\{
+\begin{aligned}
+a &= 163×40133016 \\
+b &= 163096908 \\
+τ &= 163\\
+c &= 640320^3\\ 
+\end{aligned}
+\right.
+$$
+
+可以看到没有任何根号, 都是精确的数字.
+
+接着想办法解决一大堆阶乘求和的精度问题.
+
+---
+
+令
+
+$$
+f(k)=(-1)^{k+1}\binom{2 k}{k} \binom{3 k}{k} \binom{6 k}{3 k} c^{-k}
+$$
+
+注意到两式之比可以表达成:
+
+$$
+\frac{f(n)}{f(n-1)}=\frac{8}{c}\frac{(6 n-1) (6 n-3) (6 n-5)}{n^3}
+$$
+
+于是有
+
+$$
+\begin{aligned}
+\frac{1}{π}
+=c^{-\frac{1}{2}}\left(b+\sum_{k=1}^∞\prod_{n=1}^k\left(\frac{8}{c}×\frac{(6 n-1) (6 n-3) (6 n-5)}{n^3}\right) (a k+b)\right)
+\end{aligned}
+$$
+
+又令:
+
+$$
+\begin{aligned}
+P(p,q)&=\prod_{j=p}^{q-1}(6 j-1) (6 j-3) (6 j-5)\\
+Q(p,q)&=\prod_{j=p}^{q-1}c× \left(\frac{j}{2}\right)^3 \\
+S(p,q)&=\sum_{k=p}^{q-1}\frac{P(p,k+1)}{Q(p,k+1)}(a k+b)\\
+R(p,q)&=Q(p,q) S(p,q)\\
+\end{aligned}
+$$
+
+
+以上可以重写为递推定义:
+
+$$
+\begin{aligned}
+P(p,q)&=P(p,r)×P(r,q)\\
+Q(p,q)&=Q(p,r)×Q(r,q)\\
+S(p,q)&=S(p,r)+\frac{P(p,r)}{Q(p,r)}S(r,q)\\
+R(p,q)&=Q(r,q)R(p,r) + P(p,r)R(r,q)\\
+\end{aligned}
+$$
+
+边界条件为:
+
+$$
+\begin{aligned}
+P(p,p+1)&=(6p-1)(6p-3)(6p-5)\\
+Q(p,p+1)&=\frac{cp^3}{8}\\
+S(p,p+1)&=\frac{P(p,p+1)}{Q(p,p+1)}(a p + b)\\
+R(p,p+1)&=P(p,p+1)(a p + b)\\
+\end{aligned}
+$$
+
+这个才是可编程计算的无精度损失版本.
+
+---
+
+把 S 的表达式代入原式有:
+
+$$
+\frac{1}{π}=c^{-\frac{1}{2}}\left(b+S(1,∞)\right)
+$$
+
+翻个身
+
+$$
+π=\frac{\sqrt{c}}{b+S(1,∞)}
+$$
+
+当然计算机没法算到无穷 直接截断即可, 对于 t = 163 就是:
+
+$$
+π≈\frac{5122560\sqrt{10005}}{163096908+S(1,n)}
+$$
+
+一般编程计算的是这个公式才对.
+
+---
+
+但是这个和原始的 `chudnovsky.c` 里的参数还是有区别, 这是因为
+
+
+$$
+\mathrm{GCD}(163 × 40133016, 163096908) = 12
+$$
+
+所有参数可以约去 12, 于是有:
+
+$$
+π≈\frac{426880\sqrt{10005}}{13591409+S(1,n)}
+$$
+
+这个就是超级计算机上跑的能算一万亿的版本了.
+
+当然你这个大数乘法的得分块计算, 单机没法算.
+
